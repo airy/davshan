208bedb556e9cb82b712ce092b777d225be7d789
airy <airydragon@gmail.com>
2011-07-24 05:34:48 +0300

    action mailer default url set to localhost:3000 in dev. env. (as suggested in devise install notes - #1)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,8 @@ Davshan::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  
+  # Use localhost:3000 as default url for mailer.
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
 
