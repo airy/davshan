69c13dddc9777995a69d17723e37e97b0288d20f
airy <airydragon@gmail.com>
2011-07-24 05:18:16 +0300

    Rails 3.0.9 skeleton (-JTO)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Davshan::Application.config.secret_token = 'e905e487f55b5f5a8ead3ba62037bc4d86257602b613d6e73b736ecf1fb8e91b0d9bce9ec954840262f5e90bdf0423691cb355b7f3e7853e0b28e71956b21dca'
