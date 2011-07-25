0ebe0302f95cdf34282c5e5dcb7d8072c3e842d3
airy <airydragon@gmail.com>
2011-07-25 11:43:04 +0300

    tree_select helper method added to application helper.
    TODO: refactor it and inject into ActionView FormTagHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+  
+  # TODO: refactor these ( tree_select and options_for_tree_select ) methods and inject to ActionView::Helpers::FormTagHelper
+  def tree_select(object, tree, selected = nil, include_blank = false)
+    html = ""
+    html << "<select id=\"#{object.class.name.downcase}_parent_id\" name=\"#{object.class.name.downcase}[parent_id]\" >"
+    html << "<option></option>" if include_blank
+
+    tree.roots.each do |root|
+      html << options_for_tree_select(root, selected)
+    end
+    html << "</select>"
+    html.html_safe
+  end
+
+  def options_for_tree_select(option, selected = nil)
+    html = ""
+    html << "<option value='#{option.id}' "
+    html << "selected=\"selected\"" if option.id == selected
+    html << ">#{'&nbsp;'*option.depth*2}&raquo;#{option.name}</option>"
+    if option.has_children?
+      option.children.each do |child|
+        html << options_for_tree_select(child, selected)
+      end
+    end
+    html.html_safe
+  end
 end
