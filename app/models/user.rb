34d2a2977e7302a9b91e544f75842043d9cead69
airy <airydragon@gmail.com>
2011-07-25 11:48:51 +0300

    role as finite state machine & role? method added to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User
   include Mongoid::Document
+  include Stateflow
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -21,6 +23,29 @@ class User
   validates :email, :presence => true, :uniqueness => true, :length => {:maximum => 254}, :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
   
   has_and_belongs_to_many :groups, :autosave => true, :index => true, :unique => true
+
+  field :role
+  
+  def role? name
+    self.role == name.to_s
+  end
+  
+  stateflow do
+    state_column :role
+    initial :user
+    
+    state :user, :moderator, :admin
+    
+    event :promote do
+      transitions :from => :user, :to => :moderator
+      transitions :from => :moderator, :to => :admin
+    end
+
+    event :demote do
+      transitions :from => :admin, :to => :moderator
+      transitions :from => :moderator, :to => :user
+    end
+  end
   
   def avatar_url(size = 64)
     gravatar_id = Digest::MD5.hexdigest(self.email.downcase)
